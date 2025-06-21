4451c5a73651d9c4414083ed8caebd777d1714f3
Ganeshpund7 <ganeshpund0000@gmail.com>
2025-06-22 01:14:34 +0530

    new client id

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 createRoot(document.getElementById('root')).render(
-     <GoogleOAuthProvider clientId="729952540689-n4jp9iai0mqlqvv4ose7tfg85s06no8b.apps.googleusercontent.com">
+     <GoogleOAuthProvider clientId="729952540689-1m078l5blh5gfqr0l0o9o0q5ffkblmkj.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
 )
